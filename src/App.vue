5b6e884cf6e6ee380040c74a56b8dcb1d5f4f388
somej-123 <somej@naver.com>
2024-03-17 22:52:40 +0900

    vue for문, if문, import/export 모달창 만들기 등 테스트

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,73 @@
 <template>
 
+<div class="black-bg" v-if="모달창열렸니 == true">
+  <div class="white-bg">
+    <h4>상세페이지임</h4>
+    <p>상세페이지 내용임</p>
+    <button @click="모달창열렸니=false">닫기</button>
+  </div>
+</div>
+
+
   <div class="menu">
     <a v-for="(작명) in 메뉴들" :key="작명">{{ 작명 }}</a>
   </div>
 
-  <div>
-    <h4>{{ products[0] }}</h4>
-    <p>50 만원</p>
-  </div>
-  <div>
-    <h4>{{ products[1] }}</h4>
-    <p>70 만원</p>
-  </div>
-  <div>
-    <h4>{{ products[2] }}</h4>
-    <p>90 만원</p>
+
+
+  <div v-for="oneRoomList in 원룸들" :key="oneRoomList.title">
+    <img :src="oneRoomList.image" class="room-img"/>
+    <h4 @click="모달창열렸니 = true">{{ oneRoomList.title }}</h4>
+    <p>{{ oneRoomList.price }}원</p>
+    <button @click="신고수[0]++">허위매물신고버튼</button> <span>신고수 : {{ 신고수[0] }}</span>
   </div>
 </template>
 
 <script>
 
+import data from './assets/oneroom.js';
 
 export default {
   name: 'App',
   //데이터 보관함
   data(){
     return {
+      원룸들 : data,
+      모달창열렸니 : false,
+      신고수 : [0,0,0],
       메뉴들 : ['Home', 'Shop', 'About'],
       products: ['역삼동원룸', '천호동원룸', '마포구원룸']
     }
   },
+  methods : {
+  },
+
   components: {
   }
 }
 </script>
 
 <style>
+body{
+  margin: 0;
+}
+div{
+  box-sizing: border-box;
+}
+.black-bg{
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg{
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -53,5 +85,10 @@ export default {
 .menu a{
   color: white;
   padding: 10px;
+}
+
+.room-img{
+  width: 100%;
+  margin-top: 40px;
 }
 </style>
