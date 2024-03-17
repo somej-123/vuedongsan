9e1707d8fbddf8935f4dab8c1f4621d8062f98c6
somej-123 <somej@naver.com>
2024-03-17 16:17:06 +0900

    vue 연습

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+
+  <div class="menu">
+    <a v-for="(작명) in 메뉴들" :key="작명">{{ 작명 }}</a>
+  </div>
+
+  <div>
+    <h4>{{ products[0] }}</h4>
+    <p>50 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>70 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>90 만원</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  //데이터 보관함
+  data(){
+    return {
+      메뉴들 : ['Home', 'Shop', 'About'],
+      products: ['역삼동원룸', '천호동원룸', '마포구원룸']
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.menu{
+  background: darkslateblue;
+  padding:15px;
+  border-radius: 5px;
+}
+
+.menu a{
+  color: white;
+  padding: 10px;
+}
+</style>
